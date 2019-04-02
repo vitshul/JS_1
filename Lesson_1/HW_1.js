e6cefb0952e05771e229200b0ecc73388d81af5e
vsh86 <47505733+vsh86@users.noreply.github.com>
2019-04-02 23:08:56 +0300

    инкремент и декремент

--- Lesson_1/HW_1.js
+++ Lesson_1/HW_1.js
@@ -41,8 +41,8 @@ number += 12;
 number -= 14;
 number *= 5;
 number /= 7;
-number += 1;
-number -= 1;
+number ++;
+number --;
 /*alert(number);*/
 
 var RESULT = 'ЧИСЛА\n' +
@@ -62,34 +62,11 @@ number += 12;
 number -= 14;
 number *= 5;
 number /= 7;
-number += 1;
-number -= 1;
+number ++;
+number --;
 alert(number);`+
     '\n Результат: number = '+number;
 
-
-/*
-alert('ЧИСЛА\n' +
-               'Задание 1 \n a + b = '+sum+'\n a - b = '+razn+'\n a * b = '+proizv+'\n a / b = '+del+
-             '\nЗадание 2 \n result = '+result+
-         '\nСТРОКИ'+
-               '\nЗадание 3.2 \n'+
-                 'В часе - '+secHour+' секунд, в сутках - '+secDay+' секунд, в месяце - '+secMonth1+' или '+secMonrh2+' секунд.'+
-              '\nЗадание 3.2 \n'+
-                    'Текущее время : '+hour+':'+minut+':'+second+
-                '\nЗадание 3.3 \n'+
-                     'Число \'perem\' в квадрате = '+perem+
-                 '\nЗадание 3.4 \n'+
-                      'Код с использованием сокращенной записи операций:\n'+
-`var number = 1;
-number += 12;
-number -= 14;
-number *= 5;
-number /= 7;
-number += 1;
-number -= 1;
-alert(number);`+
-        '\n Результат: number = '+number    );*/
 
 alert(RESULT);
 
